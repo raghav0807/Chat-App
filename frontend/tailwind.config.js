c7a93cb059a2a92ad3642a6db5be3025423527d6
raghav0807 <raghav.agiwal2002@gmail.com>
2024-11-26 19:05:13 +0530

    Login Page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-// eslint-disable-next-line no-undef
-module.exports = {
+export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
